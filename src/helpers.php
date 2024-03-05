420275acc71228729054dd230d0eded063fe4cb4
Mansi Prajapati <mansi@conceptualize.ae>
2024-03-05 14:14:39 +0530

    helper update

--- src/helpers.php
+++ src/helpers.php
@@ -1,9 +1,11 @@
 <?php
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+if (!function_exists('addApiLog')) {
 function addApiLog($tag,$message = '')
 {
     $log = new Logger('Api');
     $log->pushHandler(new StreamHandler(storage_path('logs/Api.log')), Logger::INFO);
     $log->info($tag, array($message));
+}
 }
